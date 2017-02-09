a0c35f106747c6d0fc8610ec3fd2ed4a4ad661cc
brandonxiang <brandonxiang@brandonxiangdeMacBook-Pro.local>
2017-02-09 23:51:41 +0800

    demo1 and 2

---
+++ src/demo2.js
@@ -0,0 +1,21 @@
+function findHighestWord(essay){
+  var wordNum = {};
+  var words = essay.split(/,|.|\s+/);
+  for(var i =0 ; i< words.length;i++){
+    var num = 0;
+    for(var j=i+1;j< words.length;j++){
+      if(words[i]===words[j]){
+        words.splice(j,1);
+        num ++;
+      }
+    }
+    wordNum[words[i]]= num;
+  }
+  var max = 0;
+  for(word in wordNum){
+    if(wordNum.word > max){
+      max = wordNum.word;
+    }
+  }
+  return max;
+}

a08cef614577c1877b85c68636be9575a45143ef
brandonxiang <brandonxiang@brandonxiangdeMacBook-Pro.local>
2017-02-19 00:40:48 +0800

    add deom4.test.js

--- test/demo4.test.js
+++ test/demo4.test.js
@@ -1,2 +1,11 @@
-var expect = require('chai').expect;
-var reUrl = require('')
+var expect = require('chai').expect
+var reUrl = require('../src/demo4')
+
+
+describe('Demo 4', function(){
+    var url = 'http://data.auto.qq.com/'
+
+    it('determine whether this url belongs to qq', function(){
+        expect(reUrl(url)).to.be.true
+    })
+})
